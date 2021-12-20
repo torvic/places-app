36d4099ef2f252fdaa44891c5ce94435fcfe4d31
torvic <130111@unsaac.edu.pe>
2021-12-19 20:07:32 -0500

    Feat(SideDrawer): Render SideDrawer as a Portal

--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -1,8 +1,10 @@
+import { createPortal } from 'react-dom';
 import './SideDrawer.css';
 
 const SideDrawer = (props) => {
   const { children } = props;
-  return <div className="side-drawer">{children}</div>;
+  const content = <aside className="side-drawer">{children}</aside>;
+  return createPortal(content, document.getElementById('side-drawer'));
 };
 
 export default SideDrawer;
